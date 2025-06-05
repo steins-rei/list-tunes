71f0e80a33dea7126b402d86d969fb1eb0799e3b
steins-rei <stienrei@gmail.com>
2025-06-05 21:01:36 +0800

    Error fix

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,6 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import global from "./globals.css"
 
-import About from "./pages/about/page";
 import Home from "./pages/home/page"
 
 export default function Page() {
